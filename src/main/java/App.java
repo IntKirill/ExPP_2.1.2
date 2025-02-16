beb3d03b5e56411c7470fc1940098d2eafcf05ec
Kirill Chuchalin <KirillizDD@yandex.ru>
2025-02-16 16:38:25 +0400

    Задание выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+        System.out.println(cat1.getName());
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
     }
 }
